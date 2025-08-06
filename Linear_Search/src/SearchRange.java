0986c6eec87f25e0b2c02ed564050737c9d59ee3
Rashmi <rashmim2405@gmail.com>
2025-08-07 00:50:03 +0530

    Searching2

--- Linear_Search/src/SearchRange.java
+++ Linear_Search/src/SearchRange.java
@@ -1,11 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class SearchRange {
     public static void main(String[] args) {
         int[] a={15,2,4,1,6};
         int x=3;
+        int start=1;
+        int end=4;
 //        System.out.print("Location: "+Linear(a,a.length,x));
-        System.out.print("Location: "+Linear2(a,a.length,x));
+        System.out.print("Location: "+Linear2(a,x,start,end));
 
     }
 
@@ -22,9 +24,13 @@ public class Main {
 //return -1;
 //    }
 
-    static int Linear2(int[] a, int n, int x){
+    static int Linear2(int[] a, int x,int beg,int end){
+        if(end>a.length || beg<0){
+            System.out.println("Invalid range");
+            return -1;
+        }
 
-        for(int i=0;i<n;i++){
+        for(int i=beg;i<end;i++){
             if(a[i]==x) {
                 System.out.println("Found");
                 return i;
--- Linear_Search/src/StringSearch.java
+++ Linear_Search/src/StringSearch.java
@@ -1,2 +1,32 @@
+import java.util.Arrays;
+
 public class StringSearch {
+    public static void main(String[] args) {
+         String st= "Rashmi";
+         char ch='f';
+       // System.out.println(foundChar(st,ch));
+
+        System.out.println(Arrays.toString(st.toCharArray()));
+        System.out.println( foundChar2(st,ch));
+    }
+
+//    static boolean foundChar(String s, char c){
+//        if(s.length()==0) return false;
+//
+//        for(int i=0;i<s.length();i++){
+//            if(s.charAt(i)==c) return true;
+//        }
+//        return false;
+//    }
+
+
+    static boolean foundChar2(String s, char c){
+        if(s.length()==0) return false;
+
+        for(char ch: s.toCharArray()){
+            if(c==ch) return true;
+        }
+
+        return false;
+    }
 }
